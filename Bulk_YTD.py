c10eaa9081cf220c8b7f3ea5ca85099ffa81b922
lukazavrtanik <luka.zavrtanik@gmail.com>
2022-01-17 21:56:29 +0100

    Fixed filename converter error

--- Bulk_YTD.py
+++ Bulk_YTD.py
@@ -3,6 +3,7 @@ import pytube #Dwonloading from YouTube
 import os #Getting current folder
 from datetime import datetime #Getting current date and time
 from moviepy.editor import * #Converting mp4 to mp3
+import glob  #For getting the latest file name
 
 #Download only videos containing one of these words - INPUT
 word1 = "mix"
@@ -37,7 +38,7 @@ currentfolder = os.getcwd()                                 #Get current folder
 currentdate = datetime.now()                                #Get current date and time - for naming the new folder
 currentdate = str(currentdate)                              #Make the date nad time into a string
 folderpat = re.compile(r":")                                #Remove colons - otherwise Windows complains
-foldername= folderpat.sub("-", currentdate)
+foldername= folderpat.sub("-", currentdate)                 #Remove colons - otherwise Windows complains
 os.mkdir(foldername)                                        #Create a new folder
 SAVE_PATH = os.path.abspath(os.getcwd()+"\\"+foldername)    #Set the save path
 os.chdir(SAVE_PATH)                                         #Move working dir to the new folder
@@ -50,12 +51,14 @@ for match in matches:
     #Check if the video title contains the set words
     if (word1 in name) or (word2 in name) or (word3 in name) or (word4 in name) or (word5 in name) or (word6 in name):
         if dp==1:
-            print(name)
-            yt.streams.get_highest_resolution().download(SAVE_PATH)
+            #print(name)
+            filtername = re.sub('[^0-9a-zA-Z\s]', '', name)  # Remove special characters - otherwise there might be an error
+            print(filtername)
+            yt.streams.get_highest_resolution().download(SAVE_PATH, filename=filtername+".mp4")
             #Converts to mp3
             if mp3 == 1:
-                oldname = name + ".mp4"
-                newname = name + ".mp3"
+                oldname = filtername + ".mp4"
+                newname = filtername + ".mp3"
                 videoclip = VideoFileClip(oldname)
                 audioclip = videoclip.audio
                 audioclip.write_audiofile(newname)
